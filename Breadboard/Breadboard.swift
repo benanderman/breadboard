fc33528f633608fdf1ee89ad67e10acaba60ca9c
Ben Anderman <bena@evidentpoint.com>
2016-05-03 01:48:55 -0400

    Add CircuitNodeGrid class, and implement Breadboard class

--- Breadboard/Breadboard.swift
+++ Breadboard/Breadboard.swift
@@ -9,9 +9,25 @@
 import Foundation
 
 class Breadboard {
+  let leftPowerRail: CircuitNodeGrid
+  let rightPowerRail: CircuitNodeGrid
+  let leftRows: CircuitNodeGrid
+  let rightRows: CircuitNodeGrid
   
-  init() {
+  let groundNode = CircuitNode()
+  let powerNode = CircuitNode()
+  
+  init(width: UInt, height: UInt) {
+    leftPowerRail = CircuitNodeGrid(width: 2, height: height, connectionType: .Columns)
+    rightPowerRail = CircuitNodeGrid(width: 2, height: height, connectionType: .Columns)
     
+    leftPowerRail.columns[0][0].connect(groundNode)
+    rightPowerRail.columns[0][0].connect(groundNode)
+    leftPowerRail.columns[0][1].connect(powerNode)
+    rightPowerRail.columns[0][1].connect(powerNode)
+    
+    leftRows = CircuitNodeGrid(width: (width - 4) / 2, height: height, connectionType: .Rows)
+    rightRows = CircuitNodeGrid(width: (width - 4) / 2, height: height, connectionType: .Rows)
   }
   
 }
---
+++ Breadboard/CircuitNodeGrid.swift
@@ -0,0 +1,51 @@
+//
+//  CircuitNodeGrid.swift
+//  Breadboard
+//
+//  Created by Ben Anderman on 5/2/16.
+//  Copyright © 2016 Ben Anderman. All rights reserved.
+//
+
+import Foundation
+
+class CircuitNodeGrid {
+  enum GridConnectionType {
+    case Rows
+    case Columns
+    case None
+  }
+  
+  var columns: [[CircuitNode]]
+  var connectionGroups: [CircuitNode]
+  
+  init(width: UInt, height: UInt, connectionType: GridConnectionType) {
+    columns = [[CircuitNode]]()
+    for _ in 0 ..< width {
+      let column = (0 ..< height).map { _ in CircuitNode() }
+      columns.append(column)
+    }
+    
+    connectionGroups = [CircuitNode]()
+    switch connectionType {
+    case .Rows:
+      for y in 0 ..< Int(height) {
+        let group = CircuitNode()
+        connectionGroups.append(group)
+        for x in 0 ..< Int(width) {
+          columns[x][y].connect(group)
+        }
+      }
+      
+    case .Columns:
+      for column in columns {
+        let group = CircuitNode()
+        connectionGroups.append(group)
+        for node in column {
+          node.connect(group)
+        }
+      }
+      
+    case .None: break
+    }
+  }
+}
